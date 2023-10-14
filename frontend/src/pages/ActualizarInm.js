542cf2493fc7d2f17292144e4f97f386d143cc39
Lesly2022 <parralesly17@gmail.com>
2023-10-14 00:18:00 -0400

    NewActInm

--- frontend/src/pages/ActualizarInm.js
+++ frontend/src/pages/ActualizarInm.js
@@ -1,8 +1,24 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {Link, Outlet, useParams } from 'react-router-dom';
 import { sitios } from '../sitios';
+import Select from 'react-select';
+
+
 const ActualizarInm = () =>{
-   
+    const [selectedServices, setSelectedServices] = useState([]);
+    const serviceOptions = [
+        { value: 'estacionamiento', label: 'Estacionamiento' },
+        { value: 'piscina', label: 'Piscina' },
+        { value: 'cocina', label: 'Cocina' },
+        { value: 'parrilla', label: 'Parrilla' },
+        { value: 'terraza', label: 'Terraza' },
+        { value: 'desayuno', label: 'Desayuno' },
+        { value: 'camara', label: 'Cámara' },
+        { value: 'llaveElectronica', label: 'Llave electrónica' },
+        { value: 'guardias', label: 'Guardias' },
+        { value: 'detectorHumo', label: 'Detector de humo' },
+      ];
+      const [isServicesOpen, setServicesOpen] = useState(false); 
         let { espaciosID } = useParams ()
 
         let espacioSeleccionado = sitios.find( site => site.id === espaciosID)
@@ -22,126 +38,97 @@ const ActualizarInm = () =>{
                                 <p class='inmCamas'>{espacioSeleccionado.camas}</p>
                                 <p class='inmPrecio'>{espacioSeleccionado.precio}</p>
                             </div>
-                            <button class="eliminar-btn" >Eliminar</button>
+                            <button className="btn-eliminar"  >Eliminar</button>
                         </div>
+
+                        
                     </div>
                     <div class="derecha">
                         <form>
                             <div class='Grid1'>
-                                <label htmlFor='tipodeinmueble'>TIPO DE INMUEBLE:</label>
-                                <select name='tipodeinmueble' placeholder={`espacioSeleccionado.precio`}>
-
-                                    <option value=''></option>
-                                    <option value='casa'>CASA</option>
-                                    <option value='habitacion'>HABITACION</option>
-                                    <option value='departamento'>DEPARTAMENTO</option>
-                                    <option value='cabaña'>CABAÑA</option>
-                                </select>
+                                <label htmlFor='idInmueble'>ID DEL INMUEBLE:</label>
+                                <input type='int' id='idInmueble' name='idInmueble' step="1"></input>
                                 <br></br>
-                                <label htmlFor='tipodealojamiento'>TIPO DE ALOJAMIENTO:</label>
-                                <select name='tipodealojamiento'>
-                                    <option value=''></option>
-                                    <option value='privado'>PRIVADO</option>
-                                    <option value='compartido'>COMPARTIDO</option>
-                                </select>
+                                <label htmlFor='TipoPropiedad'>TIPO DE PROPIEDAD:</label>
+                                <input type='string' id='TipoPropiedad' name='TipoPropiedad' step="1"></input>
+                                <br></br>
+                                <label htmlFor='TituloAnuncio'>TÍTULO DEL ANUNCIO:</label>
+                                <input type='string' id='TituloAnuncio' name='TituloAnuncio' step="1"></input>
+                                <br></br>
+                                <label htmlFor='descripcion'>DESCRIPCIÓN DETALLADA:</label>
+                                <textarea type='string' id='descripcion' name='descripcion'></textarea>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                                <label htmlFor='ubicacion'>UBICACIÓN:</label>
+                                     <button id='ubicacion' type='button' onClick={this?.onClick}>SUBIR</button>
+                                <br></br>
+                                <br></br>
                             </div>
                             <div class='Grid2'>
                                 <div class='Colum1'>
+                                    <label htmlFor='precioNoche'>PRECIO NOCHE:</label>
+                                    <input type='double' id='precioNoche' name='precioNoche' step="1"></input>
+                                    <br></br>  
                                     <label htmlFor='habitaciones'>HABITACIONES:</label>
-                                    <input type='text' id='habitaciones' name='habitaciones'></input>
+                                    <input type='int' id='habitaciones' name='habitaciones' step="1"></input>
                                     <br></br>
                                     <label htmlFor='camas'>CAMAS:</label>
-                                    <input type='text' id='camas' name='camas'></input>
+                                    <input type='number' id='camas' name='camas' step="1"></input>
                                     <br></br>
-                                    <label htmlFor='zonadetrabajo'>ZONA DE TRABAJO:</label>
-                                    <select name='zonadetrabajo'>
+                                    <label htmlFor='mascotas'>MASCOTAS:</label>
+                                    <select name='mascotas'>
                                         <option value=''></option>
                                         <option value='si'>SI</option>
                                         <option value='no'>NO</option>
                                     </select>
                                     <br></br>
-                                    <label htmlFor='lavadora'>LAVADORA:</label>
-                                    <select name='lavadora'>
-                                        <option value=''></option>
-                                        <option value='si'>SI</option>
-                                        <option value='no'>NO</option>
-                                    </select>
-                                    <br></br>
-                                    <label htmlFor='piscina'>PISCINA:</label>
-                                    <select name='piscina'>
-                                        <option value=''></option>
-                                        <option value='si'>SI</option>
-                                        <option value='no'>NO</option>
-                                    </select>
-                                    <br></br>
-                                    <label htmlFor='parrillero'>PARRILLERO:</label>
-                                    <select name='parrillero'>
-                                        <option value=''></option>
-                                        <option value='si'>SI</option>
-                                        <option value='no'>NO</option>
-                                    </select>
-                                    <br></br>
-                                    <label htmlFor='wifi'>WIFI:</label>
-                                    <select name='wifi'>
-                                        <option value=''></option>
-                                        <option value='si'>SI</option>
-                                        <option value='no'>NO</option>
-                                    </select>
+                                    <label htmlFor='calendario'> DIAS DISPONIBLES:</label>
+                                     <button id='calendario' type='button' onClick={this?.onClick}>ELEGIR</button>
                                 </div>
                                 <div class='Colum2'>
+                                    <label htmlFor='capacidad'>CAPACIDAD:</label>
+                                    <input type='int' id='capacidad' name='capacidad' step="1"></input>
+                                    <br></br> 
                                     <label htmlFor='baños'>BAÑOS:</label>
-                                    <input type='text' id='baños' name='baños'></input>
+                                    <input type='number' id='baños' name='baños' step="1"></input>
                                     <br></br>
-                                    <label htmlFor='huespedes'>HUESPEDES:</label>
-                                    <input type='text' id='huespedes' name='huespedes'></input>
-                                    <br></br>
-                                    <label htmlFor='terraza'>TERRAZA:</label>
-                                    <select name='terraza'>
+                                    <label htmlFor='niños'>NIÑOS:</label>
+                                    <select name='niños'>
                                         <option value=''></option>
                                         <option value='si'>SI</option>
                                         <option value='no'>NO</option>
                                     </select>
                                     <br></br>
-                                    <label htmlFor='jacuzzi'>JACUZZI:</label>
-                                    <select name='jacuzzi'>
-                                        <option value=''></option>
-                                        <option value='si'>SI</option>
-                                        <option value='no'>NO</option>
-                                    </select>
-                                    <br></br>
-                                    <label htmlFor='garaje'>GARAJE:</label>
-                                    <select name='garaje'>
-                                        <option value=''></option>
-                                        <option value='si'>SI</option>
-                                        <option value='no'>NO</option>
-                                    </select>
-                                    <br></br>
-                                    <label htmlFor='cocina'>COCINA:</label>
-                                    <select name='cocina'>
-                                        <option value=''></option>
-                                        <option value='si'>SI</option>
-                                        <option value='no'>NO</option>
-                                    </select>
-                                    <br></br>
-                                    <label htmlFor='television'>TELEVISION:</label>
-                                    <select name='television'>
-                                        <option value=''></option>
-                                        <option value='si'>SI</option>
-                                        <option value='no'>NO</option>
-                                    </select>
+                                    <label htmlFor='servicios'> SERVICIOS:</label>
+                                     <button id='servicios' type='button' onClick={() => setServicesOpen(!isServicesOpen)}>VER MÁS</button>
+                                     {isServicesOpen && (
+                                            <Select
+                                                isMulti
+                                                options={serviceOptions}
+                                                value={selectedServices}
+                                                onChange={setSelectedServices}
+                                            />
+                                            )}
+                                     <br></br>
+
+                                     <label htmlFor='imagenes'>IMAGENES:</label>
+                                     <button id='imagenes' type='button' onClick={this?.onClick}>SUBIR</button>
                                 </div>
                             </div>
                             <div class='Grid3'>
-                                <label htmlFor='ubicacion'>UBICACION:</label>
-                                <button id='ubicacion' type='button' onClick={this?.onClick}>SUBIR</button>
-                                <br></br>
-                                <label htmlFor='descripcion'>DESCRIPCIÓN:</label>
-                                <input type='text' id='descripcion' name='descripcion'></input>
+                                <label htmlFor='normasCasa'>NORMAS DE LA CASA:</label>
+                                <textarea type='text' id='normasCasa' name='normasCasa'></textarea>
                             </div>
-                            
                             <br></br>
-                            <div class="guardarcambios">
-                                 <button id='guardar' type='button' onClick={this?.onClick}>GUARDAR CAMBIOS</button>
+                            <br></br>
+                            <div class='BotonesInf'>
+                                     <div class="BotonCancelar">
+                                        <button id='cancelar' type='button' onClick={this?.onClick}>CANCELAR</button>
+                                    </div>
+                                    <div class="guardarcambios">
+                                        <button id='guardar' type='button' onClick={this?.onClick}>GUARDAR CAMBIOS</button>
+                                    </div>
                             </div>
 
                         </form>
@@ -154,6 +141,6 @@ const ActualizarInm = () =>{
        
          
         )
-      
+    
   }
   export default ActualizarInm;
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,13 +1,88 @@
 import React, {Component} from 'react';
 import { Outlet } from 'react-router-dom';
 class RegisterPage extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Datos de registro:', this.state);
+  }
+
+  
     render(){
      
       
       return(
         <>
-        <h2>Register</h2>
-        
+        <div class='RegistroUsuario'>
+        <h2>Registrarme</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>
+            <label>Nombre:</label>
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>
+            <label>Apellido:</label>
+            <input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>
+            <label>Correo electrónico:</label>
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>
+            <label>Teléfono:</label>
+            <input
+              type="number"
+              name="phone"
+              value={this.state.phone}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <br></br>
+          <h5>Al seleccionar Aceptar y continuar, acepto los Términos de servicio, los Términos de pago del servicio 
+            y la Política contra la discriminación de Airbnb. También reconozco la Política de privacidad.</h5>
+            <br></br>
+          <button type="submit">Aceptar</button>
+        </form>
+      </div>
         </>
         
       );
