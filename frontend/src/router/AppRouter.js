842477395fc23094a6534c2c2f7f76b301db8c96
Lesly2022 <parralesly17@gmail.com>
2023-10-17 00:05:46 -0400

    RegistroInmueble

--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -7,6 +7,7 @@ import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
 import DashboardPage from '../pages/DashboardPage';
 import { default as ActualizarInm } from '../pages/ActualizarInm';
+import { default as RegisterInmue } from '../pages/RegisterInmue';
 import { default as EspaciosModAnf } from '../pages/EspaciosModAnf';
 import NavbarCli from '../components/navBarCli';
 
@@ -28,7 +29,7 @@ class AppRouter extends Component{
             <Route index element={<EspaciosModAnf />} />
             <Route path= "/cliente/:espaciosID" element ={<ActualizarInm  />}/>
             <Route path='inm' element={<ActualizarInm />} />
-            
+            <Route path="/cliente/registerinmue" element={<RegisterInmue />} />
             <Route path='dashboard' element={<DashboardPage />} />
             <Route path='*' element={<Navigate TO ="/"/>}/> 
       </Route>
