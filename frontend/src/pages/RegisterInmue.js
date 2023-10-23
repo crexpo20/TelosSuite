52f51e96fd4668a9e05c9a5fbba5c7b542f2dc73
JulieOjed <jhuli1010ojeda@gmail.com>
2023-10-23 01:50:21 -0400

    registrInmiconos

--- frontend/src/pages/RegisterInmue.js
+++ frontend/src/pages/RegisterInmue.js
@@ -8,6 +8,35 @@ import departamentoImage from '../iconos/departamento.png';
 import cabanaImage from '../iconos/cabana.png';
 import wifiImage from '../iconos/wifi.png';
 import privadoImage from '../iconos/privado.png';
+import compartidoImage from '../iconos/compartido.png';
+import aireconImage from '../iconos/aireacon.png';
+import batidoraImage from '../iconos/batidora.png';
+import billarImage from '../iconos/billar.png';
+import camaraImage from '../iconos/camara.png';
+import cocinaImage from '../iconos/cocina.png';
+import desayunoImage from '../iconos/desyuno.png';
+import DetectorhumoImage from '../iconos/Detectorhumo.png';
+import gymImage from '../iconos/gym.png';
+import jacuzziImage from '../iconos/jacuzzi.png';
+import lavavajillaImage from '../iconos/lavavajilla.png';
+import licuadoraImage from '../iconos/licuadora.png';
+import llaveImage from '../iconos/llave.png';
+import mascotaImage from '../iconos/mascota.png';
+import microondasImage from '../iconos/microondas.png';
+import niñoImage from '../iconos/niño.png';
+import parqueoImage from '../iconos/parqueo.png';
+import parrillaImage from '../iconos/parrilla.png';
+import piscinaImage from '../iconos/piscina.png';
+import refrigeradorImage from '../iconos/refrigerador.png';
+import terrazaImage from '../iconos/terraza.png';
+import tvImage from '../iconos/tv.png';
+import vigilanciaImage from '../iconos/vigilancia.png';
+
+import chimeneaImage from '../iconos/chimenea.png';
+import ciudadImage from '../iconos/CIUDAD.png';
+
+
+
 class RegisterInmue extends Component {
   constructor(props) {
     super(props);
@@ -15,10 +44,14 @@ class RegisterInmue extends Component {
     // Define las preguntas y opciones para cada parte del carrusel
     this.slides = [
       {
+        question: 'Ingrese el nombre del Inmueble',
+        inputType: 'text', // Campo de entrada de texto
+      },
+      {
         question: '¿Cuál de estas opciones describe mejor tu espacio?',
         options: [
           { value: 'Casa', imageSrc: casaImage },
-          { value: 'Habitacion', imageSrc: habitacionImage },
+          { value: 'Habitación', imageSrc: habitacionImage },
           { value: 'Departamento', imageSrc: departamentoImage },
           { value: 'Cabaña', imageSrc: cabanaImage },
         ],
@@ -28,106 +61,181 @@ class RegisterInmue extends Component {
         question: '¿Qué tipo de alojamiento ofreces?',
         options: [
           { value: 'Privado', imageSrc: privadoImage },
-          { value: 'Compartido', imageSrc: 'compartido.png' },
+          { value: 'Compartido', imageSrc: compartidoImage },
         ],
         selectedOption: null,
       },
       {
-        question: '¿Qué tipo de servicio ofreces?',
+        question: 'Especificaciones básicas sobre el espacio y el precio',
+        fields: [
+          { label: 'Número de Habitaciones', inputType: 'number' },
+          { label: 'Número de Camas', inputType: 'number' },
+          { label: 'Número de Baños', inputType: 'number' },
+          { label: 'Número de Huéspedes', inputType: 'number' },
+        ],
+      },
+      {
+        question: '¿Tu inmueble permite ...... ?',
         options: [
+          { value: 'Niños', imageSrc: niñoImage },
+          { value: 'Mascotas', imageSrc: mascotaImage },
+        ],
+        selectedOption: null,
+      },
+      {
+        question: 'Servicios',
+        options: [
+          { value: 'Piscina', imageSrc: piscinaImage },
           { value: 'Wifi', imageSrc: wifiImage },
-          { value: 'Parrillero', imageSrc: 'parrillero.png' },
-          { value: 'Piscina', imageSrc: 'piscina.png' },
-          { value: 'Garaje', imageSrc: 'garaje.png' },
+          { value: 'Desayuno', imageSrc: desayunoImage },
+          { value: 'Estacionamiento', imageSrc: parqueoImage },
+          { value: 'TV', imageSrc:tvImage },
+          { value: 'Aire Acondicionado', imageSrc: aireconImage },
         ],
         selectedOption: null,
       },
       {
-        question: '¿Qué electrodomesticos ofreces?',
+        question: 'Servicios Destacados',
         options: [
-          { value: 'TV', imageSrc: './tv.png' },
-          { value: 'Lavadora', imageSrc: 'lavadora.png' },
+          { value: 'Jacuzzi', imageSrc: jacuzziImage },
+          { value: 'Parrilero', imageSrc: parrillaImage },
+          { value: 'Terraza', imageSrc: terrazaImage },
+          { value: 'Chimenea Interior', imageSrc: chimeneaImage },
+          { value: 'Equipo de gym', imageSrc: gymImage },
+          { value: 'Mesa Billar', imageSrc: billarImage },
         ],
         selectedOption: null,
       },
       {
-        question: 'Ingrese la ubicación exacta ',
-        inputType: 'text', // Campo de entrada de texto
-        inputValue: '', // Valor inicial del campo de entrada
+        question: '¿Qué electrodomésticos ofreces?',
+        options: [
+          { value: 'Licuadora', imageSrc: licuadoraImage },
+          { value: 'Refrigerador', imageSrc: refrigeradorImage },
+          { value: 'Lavavajillas', imageSrc: lavavajillaImage },
+          { value: 'Batidora', imageSrc: batidoraImage },
+          { value: 'Cocina', imageSrc: cocinaImage },
+          { value: 'Microondas', imageSrc: microondasImage },
+        ],
+        selectedOption: null,
+      },
+      {
+        question: '¿Cuentas con alguno de estos elementos de seguridad?',
+        options: [
+          { value: 'Cámara de Seguridad', imageSrc: camaraImage },
+          { value: 'Llaves Electrónicas', imageSrc: llaveImage },
+          { value: 'Vigilancia 24 horas', imageSrc: vigilanciaImage },
+          { value: 'Detector de Humo', imageSrc: DetectorhumoImage },
+        ],
+        selectedOption: null,
+      },
+      {
+        question: 'Ingrese la ubicación exacta',
+        fields: [
+          { label: 'Link de Ubicación', inputType: 'text' },
+          { label: 'Dirección', inputType: 'text' },
+        ],
+      },
+      {
+        question: '¿En qué departamento se encuentra el inmueble ?',
+        options: [
+          { value: 'Cochabamba', imageSrc: ciudadImage },
+          { value: 'La Paz' , imageSrc: ciudadImage},
+          { value:'Santa Cruz' , imageSrc: ciudadImage},
+          {value: 'Sucre' , imageSrc: ciudadImage},
+          { value: 'Pando' , imageSrc: ciudadImage},
+          { value: 'Tarija', imageSrc: ciudadImage },
+          { value: 'Oruro' , imageSrc: ciudadImage},
+          { value: 'Potosi', imageSrc: ciudadImage },
+          { value: 'Beni' , imageSrc: ciudadImage},
+        ],
+        selectedOption: null,
       },
       {
         question: 'Ingresar imágenes',
         inputType: 'file',
         uploadedImages: [
           { image: null, description: '' }, // Objeto para la primera imagen
-         
         ],
       },
-
       {
         question: 'Ingrese una breve descripción que sea atractiva para los huéspedes',
         inputType: 'text', // Campo de entrada de texto
-        inputValue: '', 
-      },
-      {
-        question: 'Es importante que los huéspedes se sientan seguros, selecciona las especificaciones de seguridad con las que cuente tu espacio',
-        options: [
-          { value: 'Camara de Seguridad', imageSrc: './camara.png' },
-          { value: 'Llaves Electronicas', imageSrc: 'llaves.png' },
-          { value: 'Vigilancia 24 horas', imageSrc: 'vigilancia.png' },
-        ],
-        selectedOption: null,
       },
       {
         question: 'Ingrese el precio',
         inputType: 'number', // Tipo de entrada para ingresar el precio (number para números)
-        inputValue: '',
+        pattern: '^[0-9,$]*$',
       },
       {
-        question: 'Creemos que los métodos de pago deben ser simples pero confiables, ingresa una imagen para que los huéspedes puedan verla',
+        question: 'Creemos que los métodos de pago deben ser simples pero confiables, ingresa una imagen QR para que los huéspedes puedan verla',
         inputType: 'file', // Campo de entrada de tipo archivo para la imagen
         uploadedImage: null, // Imagen seleccionada
       },
       {
         question: '¡Eso es todo! Ahora espera a que lleguen las solicitudes...',
-        linkText: 'Ir al home', // Texto del botón para el enlace
-        linkUrl: 'https://tu-enlace.com',
+        button: {
+          text: 'Ir al inicio',
+        },
       },
     ];
 
     this.state = {
       currentSlide: 0,
+      dynamicInputs: [], 
     };
   }
 
-
-  
   handleOptionSelect = (optionIndex) => {
     const { currentSlide } = this.state;
-    this.slides[currentSlide].selectedOption = optionIndex;
+    const updatedSlides = [...this.slides];
+    updatedSlides[currentSlide].selectedOption = optionIndex;
 
     if (currentSlide < this.slides.length - 1) {
       this.slider.slickNext();
-      this.setState({ currentSlide: currentSlide + 1 });
+      this.setState({ currentSlide: currentSlide + 1, slides: updatedSlides });
     }
   };
 
   handleInputChange = (event) => {
     const { currentSlide } = this.state;
     const inputValue = event.target.value;
+    const updatedSlides = [...this.slides];
+    updatedSlides[currentSlide].inputValue = inputValue;
+    this.setState({ slides: updatedSlides });
+  };
 
-    // Actualiza el valor del campo de entrada
-    this.slides[currentSlide].inputValue = inputValue;
-  };
   handleImageUpload = (event) => {
+    const { currentSlide } = this.state;
     const image = event.target.files[0];
-    this.setState({ uploadedImage: image });
+
+    if (image) {
+      const uploadedImages = [...this.slides[currentSlide].uploadedImages];
+      uploadedImages[0] = { image, description: uploadedImages[0].description };
+      const updatedSlides = [...this.slides];
+      updatedSlides[currentSlide] = {
+        ...updatedSlides[currentSlide],
+        uploadedImages,
+      };
+
+      this.setState({
+        uploadedImage: image,
+        slides: updatedSlides,
+      });
+    }
   };
+
   handleDescriptionChange = (event, index) => {
     const { uploadedImages } = this.state;
     uploadedImages[index].description = event.target.value;
     this.setState({ uploadedImages });
   };
+
+  handleButtonClick = () => {
+    // Redirigir al usuario a la ruta especificada
+    const { route } = this.slides[this.slides.length - 1].button;
+    window.location.href = route;
+  };
+
   render() {
     const { currentSlide } = this.state;
     const settings = {
@@ -144,42 +252,67 @@ class RegisterInmue extends Component {
         <Slider {...settings}>
           {this.slides.map((slide, index) => (
             <div key={index}>
-              <h5>{slide.question}</h5>
-              <div className="carousel-button-container">
-                {slide.options ? (
-                  slide.options.map((option, optionIndex) => (
-                    <button
-                      key={optionIndex}
-                      className={`custom-button ${
-                        slide.selectedOption === optionIndex ? 'active' : ''
-                      }`}
-                      onClick={() => this.handleOptionSelect(optionIndex)}
-                    >
-                     <img src={option.imageSrc} alt={option.value} className="wifi-image"   />
-                      {option.value}
-                    </button>
-                  ))
-                ) : (
-                  <>
-                    <input
-                      type={slide.inputType}
-                      value={slide.inputValue}
-                      onChange={this.handleInputChange}
-                      placeholder={slide.question}
-                    />
-                    {slide.inputType === 'file' && (
-                      <input
-                        type="file"
-                        accept="image/*" // Asegura que solo se permitan archivos de imagen
-                        onChange={this.handleImageUpload}
-                      />
-                    )}
-                  </>
-                )}
+              <div className="carousel-question">
+                <h5>{slide.question}</h5>
+                <div className="question-content">
+                  {slide.options ? (
+                    slide.options.map((option, optionIndex) => (
+                      <button
+                        key={optionIndex}
+                        className={`custom-button ${
+                          slide.selectedOption === optionIndex ? 'active' : ''
+                        }`}
+                        onClick={() => this.handleOptionSelect(optionIndex)}
+                      >
+                        <img src={option.imageSrc} alt={option.value} className="iconos" />
+                        {option.value}
+                      </button>
+                    ))
+                  ) : (
+                    <>
+                      {slide.fields ? (
+                        slide.fields.map((field, fieldIndex) => (
+                          <div key={fieldIndex} className="field-container">
+                            <label className="custom-label">{field.label}</label>
+                            <input
+                              type={field.inputType}
+                              value={slide.inputValue} // Ajusta el valor correcto si es necesario
+                              onChange={this.handleInputChange}
+                              placeholder={field.label}
+                              className="custom-input"
+                            />
+                          </div>
+                        ))
+                      ) : (
+                        <>
+                          {slide.inputType === 'file' ? (
+                            <div className="file-input-container">
+                              
+                              <input type="file" onChange={this.handleImageUpload} className="custom-file-input" />
+                            </div>
+                          ) : (
+                            <input
+                              type={slide.inputType}
+                              value={slide.inputValue}
+                              onChange={this.handleInputChange}
+                              placeholder={slide.question}
+                            />
+                          )}
+                        </>
+                      )}
+                    </>
+                  )}
+                </div>
               </div>
             </div>
           ))}
         </Slider>
+
+        {currentSlide === this.slides.length - 1 && (
+          <button onClick={this.handleButtonClick}>
+            {this.slides[currentSlide].button.text}
+          </button>
+        )}
       </div>
     );
   }
