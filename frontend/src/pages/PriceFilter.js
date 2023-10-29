d1fae81ea27713e65c7d5455c87527975b8d034d
konny98 <konnykonny1998@gmail.com>
2023-10-29 16:14:39 -0400

    Mejorar fronted Filtro Precios

--- frontend/src/pages/PriceFilter.js
+++ frontend/src/pages/PriceFilter.js
@@ -1,77 +1,93 @@
-// Importamos React y useState desde 'react' para crear nuestro componente y manejar el estado
-import React, { useState } from 'react';
-// Importamos el archivo CSS para darle estilos a nuestro componente
+// Importando el icono de cerrar de la librería React Icons y los hooks necesarios de React
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import React, { useState, useEffect } from 'react';
+// Importando el archivo de estilos CSS
 import '../CSS/PriceFilters.css';
 
-// Definimos el componente funcional PriceFilter
 function PriceFilter(props) {
-  // Definimos los estados para los precios mínimo y máximo y para mostrar u ocultar el filtro
+  // Estados para manejar los precios mínimo y máximo, y para controlar la visibilidad del filtro
   const [minPrice, setMinPrice] = useState('');
   const [maxPrice, setMaxPrice] = useState('');
   const [showFilter, setShowFilter] = useState(true);
 
-  // Función para manejar los cambios en el precio mínimo
+  // Efecto para cargar los precios guardados en el localStorage cuando el componente se monta
+  useEffect(() => {
+    const storedMinPrice = localStorage.getItem('minPrice');
+    const storedMaxPrice = localStorage.getItem('maxPrice');
+
+    if (storedMinPrice) setMinPrice(storedMinPrice);
+    if (storedMaxPrice) setMaxPrice(storedMaxPrice);
+  }, []);
+
+  // Funciones para manejar los cambios en los inputs de precios mínimo y máximo
   const handleMinPriceChange = (e) => {
     setMinPrice(e.target.value);
   };
 
-  // Función para manejar los cambios en el precio máximo
   const handleMaxPriceChange = (e) => {
     setMaxPrice(e.target.value);
   };
 
-  // Función para manejar el click en el botón de aceptar
-  const handleFilterClick = () => {
-    console.log('Precio mínimo:', minPrice);
-    console.log('Precio máximo:', maxPrice);
-  };
-
-  // Función para manejar el click en el botón de cerrar y ocultar el filtro
+  // Función para cerrar el filtro de precios
   const handleCloseClick = () => {
     setShowFilter(false);
   };
 
-  // Renderizamos el componente
+  // Función para manejar la submit del formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Precio mínimo:', minPrice);
+    console.log('Precio máximo:', maxPrice);
+    // Aquí puedes agregar la lógica para filtrar y actualizar la vista
+    // Por ejemplo: props.updateResults(minPrice, maxPrice)
+    setShowFilter(false);
+  };
+
+  // Renderizado condicional: Si showFilter es true, muestra el filtro de precios
   return (
-    // Mostramos el filtro si showFilter es true
     showFilter && (
       <div className="price-filter-container">
         <div className="price-filter-popup">
-          {/* Botón para cerrar el filtro */}
-          <button className="close-button" onClick={handleCloseClick}>
-            X
-          </button>
-          
-          {/* Título del filtro */}
-          <h2 className="jeju-font">Rango de Precios</h2>
-          {/* Contenedor para los inputs de precio mínimo y máximo */}
-          <div className="price-inputs">
-            {/* Input para el precio mínimo */}
-            <label htmlFor="minPrice">Mínimo:</label>
-            <input
-              type="number"
-              id="minPrice"
-              value={minPrice}
-              onChange={handleMinPriceChange}
-              placeholder="Precio mínimo"
-            />
-            {/* Input para el precio máximo */}
-            <label htmlFor="maxPrice">Máximo:</label>
-            <input
-              type="number"
-              id="maxPrice"
-              value={maxPrice}
-              onChange={handleMaxPriceChange}
-              placeholder="Precio máximo"
-            />
+          <div className="filter-header">
+            <span className="filter-title">Filtro</span>
+            {/* Botón para cerrar el filtro de precios */}
+            <button className="close-button" onClick={handleCloseClick}>
+              <AiOutlineCloseCircle />
+            </button>
           </div>
-          {/* Botón para aceptar y aplicar el filtro */}
-          <button onClick={handleFilterClick}>Aceptar</button>
+          <div className="line"></div>
+          <h2>Rango de Precios</h2>
+          {/* Formulario para ingresar los precios mínimo y máximo */}
+          <form onSubmit={handleSubmit}>
+            <div className="price-inputs">
+              <div className="input-group">
+                <label htmlFor="minPrice">Mínimo:</label>
+                <input
+                  type="number"
+                  id="minPrice"
+                  value={minPrice}
+                  onChange={handleMinPriceChange}
+                  placeholder="Precio mínimo"
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="maxPrice">Máximo:</label>
+                <input
+                  type="number"
+                  id="maxPrice"
+                  value={maxPrice}
+                  onChange={handleMaxPriceChange}
+                  placeholder="Precio máximo"
+                />
+              </div>
+            </div>
+            <button type="submit" className="submit-button">Aceptar</button>
+          </form>
         </div>
       </div>
     )
   );
 }
 
-// Exportamos el componente para poder usarlo en otros archivos
+// Exportando el componente para poder usarlo en otros archivos
 export default PriceFilter;
