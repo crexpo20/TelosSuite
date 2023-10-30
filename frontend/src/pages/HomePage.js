53941e02a871900b2d1cd923f013602b1a27e7be
konny98 <konnykonny1998@gmail.com>
2023-10-30 00:37:49 -0400

    Inmueble homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,45 +1,47 @@
-import React, {Component} from 'react';
-import {Link, Outlet } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 import { sitios } from '../sitios';
+import '../CSS/cards.css';
 import { inmuebles } from '../components/inmuebles';
-class HomePage extends Component{
-    render(){
-     
-      
-      return(
-        <>
-          
-        <body>
-           <div>
-                 
-           { inmuebles.map((sitio, index) => {
-                            if(localStorage.getItem("mascotas") === "0" && localStorage.getItem("niños") === "0" && localStorage.getItem("huespedes") === "1" && localStorage.getItem("tipo") === "cualquiera" && localStorage.getItem("destino") === "Cualquier Lugar"){ 
-                                return(
-                                  <div class='verinm' key = {sitio.id}>
-                                  <div class='InmueblesHost'>
-                                  <img class='inmueble_fot' src="https://picsum.photos/280/280"></img>
-                                  
-                                  <h3 class='inmueble_name'>{sitio.tituloanuncio}</h3>
-                                  <div class='inmueble_info'>
-                                      <p class='inmDet'>{sitio.descripcion}</p>
-                                      <p class='inmCamas'>{sitio.camas}</p>
-                                      <p class='inmPrecio'>{sitio.precio}</p>
-                                  </div>
-                                  
-                              </div>
-                              </div>
-                                )
-                            }
-                        })
 
-                  }
-              </div>
-                  
+class HomePage extends Component {
+  render() {
+    return (
+      <>
+        <body>
+          <div className="verinm">
+            {inmuebles.map((sitio, index) => {
+              if (
+                localStorage.getItem("mascotas") === "0" &&
+                localStorage.getItem("niños") === "0" &&
+                localStorage.getItem("huespedes") === "1" &&
+                localStorage.getItem("tipo") === "cualquiera" &&
+                localStorage.getItem("destino") === "Cualquier Lugar"
+              ) {
+                return (
+                  <div className="InmueblesHost" key={sitio.id}>
+                    <img
+                      className="inmueble_fot"
+                      src="https://picsum.photos/280/280"
+                      alt="Inmueble"
+                    />
+                    <h3 className="inmueble_name">{sitio.tituloanuncio}</h3>
+                    <div className="inmueble_info">
+                      <p className="inmDet">{sitio.descripcion}</p>
+                      <p className="inmCamas">{sitio.camas}</p>
+                      <p className="inmPrecio">{sitio.precio}</p>
+                    </div>
+                  </div>
+                );
+              }
+              return null;
+            })}
+          </div>
         </body>
-        
         <Outlet />
-        </>
-      );
-    }
+      </>
+    );
   }
-  export default HomePage;
+}
+
+export default HomePage;
