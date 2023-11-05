c09a5c193b0f13fb3eb2cabf62126f0002d5d80f
Lesly2022 <parralesly17@gmail.com>
2023-11-05 08:57:09 -0400

    Favoritos

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,17 +3,47 @@ import { Link, Outlet } from 'react-router-dom';
 import { sitios } from '../sitios';
 import '../CSS/cards.css';
 import { inmuebles } from '../components/inmuebles';
+import iconocorazon1 from '../iconos/corazon1.png';
+import iconocorazon2 from '../iconos/corazon2.png';
+
 import axios from "axios";
 class HomePage extends Component {
   constructor(props){
     super(props);
     this.state={
-      inmueble:[]
+      inmueble:[],
+      favorites: [],
     }
     this.getProductos = this.getProductos.bind(this);
     
 }
- 
+
+/*añadir favoritos*/
+toggleFavorite = (sitio) => {
+  const { favorites } = this.state;
+  const sitioId = sitio.idinmueble;
+
+  if (favorites.includes(sitioId)) {
+    const updatedFavorites = favorites.filter((id) => id !== sitioId);
+    this.setState({ favorites: updatedFavorites }, () => {
+      console.log('Favoritos actualizados:', this.state.favorites);
+    });
+  } else {
+    this.setState(
+      { favorites: [...favorites, sitioId] },
+      () => {
+        console.log('Favoritos actualizados:', this.state.favorites);
+      }
+    );
+  }
+};
+
+getFavoritos() {
+  return this.state.favorites;
+}
+
+
+
 componentDidMount(){
   this.getProductos();
  
@@ -29,13 +59,22 @@ getProductos=async()=>{
   });
 }
 
+
   render() {
+    const { favorites } = this.state;
+
+    const inmueblesFavoritos = this.state.inmueble.filter((sitio) =>
+    favorites.includes(sitio.idinmueble)
+    );
     return (
       <>
         <body>
           <div className="verinm">
             {this.state.inmueble.map((sitio, index) => {
-               return (
+
+            const isFavorite = favorites.includes(sitio.idinmueble);
+
+              return (
                 <div className="InmueblesHost" key={sitio.id}>
                   <img
                     className="inmueble_fot"
@@ -44,13 +83,38 @@ getProductos=async()=>{
                   />
                   <h3 className="inmueble_name">{sitio.tituloanuncio}</h3>
                   <div className="inmueble_info">
-                    <p className="inmDet">{sitio.ciudad}</p>
+                    <p className="inmDet">{sitio.ciudad}</p> 
                     <p className="inmCamas">{sitio.camas}</p>
                     <p className="inmPrecio">{sitio.precio}</p>
                   </div>
+
+                  <button
+                             onClick={() => this.toggleFavorite(sitio)}
+                             
+                             className={isFavorite ? 'favorite-button active' : 'favorite-button'}
+                           >
+                         <img
+                            src={isFavorite ? iconocorazon2 : iconocorazon1}
+                            alt={isFavorite ? 'Quitar de Favoritos' : 'Agregar a Favoritos'}
+                        
+                      />
+                    </button>
+                    <favoritos favorites={this.state.inmueblesFavoritos} />
+
                 </div>
-              );
-               })}
+              )
+              return null;
+            })}
+          </div>
+         
+         
+<div className="favorites-list">
+            <h2>Favoritos:</h2>
+            <ul>
+              {inmueblesFavoritos.map((sitio) => (
+                <li key={sitio.idinmueble}>{sitio.tituloanuncio}</li>
+              ))}
+            </ul>
           </div>
         </body>
         <Outlet />
--- frontend/src/pages/VistaDetalladaInm.js
+++ frontend/src/pages/VistaDetalladaInm.js
@@ -4,8 +4,43 @@ import { sitios } from '../sitios';
 import { inmuebles } from '../components/inmuebles';
 import axios from "axios";
 class VistaDetalladaInm extends Component {
+
+  state = {
+    currentImageIndex: 0,
+    imageCarouselOpen: false,
+    images: [
+      'https://picsum.photos/280/280',
+      'https://picsum.photos/280/280',
+    ],
+    imageDescriptions: [
+      'Descripción de la imagen 1',
+      'Descripción de la imagen 2',
+    ],
+  };
+  openImageCarousel = () => {
+    this.setState({ imageCarouselOpen: true });
+  };
+
+  closeImageCarousel = () => {
+    this.setState({ imageCarouselOpen: false });
+  };
+
+  showNextImage = () => {
+    this.setState((prevState) => ({
+      currentImageIndex: (prevState.currentImageIndex + 1) % prevState.images.length,
+    }));
+  };
+
+  showPreviousImage = () => {
+    this.setState((prevState) => ({
+      currentImageIndex:
+        (prevState.currentImageIndex - 1 + prevState.images.length) % prevState.images.length,
+    }));
+  };
+
  
   render() {
+    
     return (
       <>
         <body>
@@ -20,7 +55,10 @@ class VistaDetalladaInm extends Component {
             </div>
             <div className='Columna3'>
                 <img src="https://picsum.photos/280/280" alt='Imagen 3-1' />
+                <div style={{ position: 'relative' }}>
                 <img src="https://picsum.photos/280/280" alt='Imagen 3-2' />
+                <button onClick={this.openImageCarousel} className="overlay-button">Ver carrusel</button>
+              </div>
             </div>
         </div>
         <div className='GridInformacion'>
@@ -28,7 +66,11 @@ class VistaDetalladaInm extends Component {
                 <h className='title1'>Casa de huéspedes - Anfitrión: Martin</h>
                 <h className='title2'>3 huéspedes - 1 habitación - 2 camas - 1 baño privado</h>
                 <br></br>
+                <div className="divisor-plomo"></div>
+                <br></br>
+                <div className='informacionAdicional'>
                 <h className='title3'>Relájate en esta escapada única y tranquila. Somos una casa de Campo de la comunidad Yumani. Esta habitacion viene con baño privado y una vista increible al lago. Nuestro estilo es de origen Aymara, con objetos y pinturas de inspiración indígena ancestral. Disponemos de Servicio de Restaurante. Atendido por Martín y Justina, quienes te darán las mejores indicaciones para explorar la Isla sagrada.</h>
+                </div>
             </div>
             <div className='Colum2'>
 
@@ -36,6 +78,17 @@ class VistaDetalladaInm extends Component {
         </div>
         </body>
         <Outlet />
+        {this.state.imageCarouselOpen && (
+          <div className="image-carousel">
+            <img src={this.state.images[this.state.currentImageIndex]} alt='Imagen Carrusel' />
+            <p className="image-description">
+              {this.state.imageDescriptions[this.state.currentImageIndex]}
+            </p>
+            <button onClick={this.showPreviousImage}>Anterior</button>
+            <button onClick={this.showNextImage}>Siguiente</button>
+            <button onClick={this.closeImageCarousel}>Cerrar Carrusel</button>
+          </div>
+        )}
       </>
     );
   }
