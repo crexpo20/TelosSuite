226794caa324995450ec7ebee81b3104548218ef
crexpo20 <crespomauricio16@gmail.com>
2023-11-11 22:33:46 -0400

    ASA

--- backend/app/Models/favorito.php
+++ backend/app/Models/favorito.php
@@ -10,6 +10,15 @@ class favorito extends Model
     use HasFactory;
     protected $table = 'favorito';
     protected $primaryKey = 'idfav';
-    protected $fillable = ['idhuesped','idinmueble'];
+    protected $fillable = ['idusuario','idinmueble'];
     public $timestamps = false;
+
+public function Usuario(){
+    return $this->hasMany(usuario::class,'idusuario');
 }
+
+public function Inmueble(){
+    return $this->hasMany(Comentario::class,'idinmueble');
+}
+}
+
