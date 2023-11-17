3ba89368925c34ff1cc0191809d206dbc3c83080
Lesly2022 <parralesly17@gmail.com>
2023-11-17 13:51:40 -0400

    Act

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -116,6 +116,9 @@ class HomePage extends Component {
                       alt={isFavorite ? 'Quitar de Favoritos' : 'Agregar a Favoritos'}
                     />
                   </button>
+                  <div className='BotonMasDetalles'>
+                      <Link to={`/cliente/${sitio.idinmueble}`}>Ver más</Link>
+                    </div>
                 </div>
               );
             })}
